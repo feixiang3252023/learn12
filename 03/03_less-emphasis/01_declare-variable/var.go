73ebabe211a4fbfc99412182649d448a667c09d8
feixiang3252023 <feixiang3252023@163.com>
2024-08-12 22:46:20 +0800

    定义主程序，输出字符串

---
+++ 03/03_less-emphasis/01_declare-variable/var.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message string
+	//声明了一个变量 message，类型为 string（字符串），用来存储文本信息。
+	message = "Hello World."
+	//将字符串 "Hello World." 赋值给变量 message
+	fmt.Println(message)
+	//使用 fmt.Println 函数将 message 变量的内容打印到控制台
+}
