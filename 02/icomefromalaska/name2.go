d924a3a2c97b63f9e7843d23ba12cc85857fba5b
feixiang3252023 <feixiang3252023@163.com>
2024-08-12 21:36:03 +0800

    可供外部使用的变量

---
+++ 02/icomefromalaska/name2.go
@@ -0,0 +1,5 @@
+package winniepooh //创建了一个名为 winniepooh 的包
+
+// MyName will be exported because it starts with a capital letter.
+var BearName = "Pooh" //定义了一个可供外部使用的变量 BearName，它的值是 "Pooh"
+//允许其他导入此包的代码访问 BearName 变量。
