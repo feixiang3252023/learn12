01cdb3f77aee5fbdf027e16495fe5fcd0b05c70c
feixiang3252023 <feixiang3252023@163.com>
2024-08-13 19:01:21 +0800

    指定类型为int

---
+++ 03/03_less-emphasis/03_init-many-at-once/var.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var message = "Hello World!"
+	var a, b, c int = 1, 2, 3
+	//声明变量 a, b, c，并指定它们的类型为 int（整数），然后分别赋值为 1, 2, 3
+	fmt.Println(message, a, b, c)
+}
