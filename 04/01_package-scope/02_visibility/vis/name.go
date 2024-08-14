fdc757395c4e9d29754287d89a331ac5e989b34d
feixiang3252023 <feixiang3252023@163.com>
2024-08-14 22:19:51 +0800

    可导出的公共变量

---
+++ 04/01_package-scope/02_visibility/vis/name.go
@@ -0,0 +1,13 @@
+package vis
+
+//定义了一个名为 vis 的包
+//包是 Go 语言中代码的组织单元，用于封装相关的功能和变量
+
+// MyName is exported because it starts with a capital letter
+var MyName = "Todd"
+
+//声明了一个公共变量 MyName
+//首字母是大写的，它是导出的（可见的），可以在其他包中访问。
+var yourName = "Future Rock Star Programmer"
+
+//yourName 的初始值为 "Future Rock Star Programmer"
