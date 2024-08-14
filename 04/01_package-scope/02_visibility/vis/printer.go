490f8284e29efb9529efbfc663ec43bac82cadd8
feixiang3252023 <feixiang3252023@163.com>
2024-08-14 22:21:07 +0800

    公共函数

---
+++ 04/01_package-scope/02_visibility/vis/printer.go
@@ -0,0 +1,10 @@
+package vis
+
+import "fmt"
+
+// PrintVar is exported because it starts with a capital letter
+func PrintVar() {
+	fmt.Println(MyName)
+	//打印 MyName 变量的值。
+	fmt.Println(yourName)
+}
